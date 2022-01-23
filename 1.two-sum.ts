f02192dbc0dc46684c0c4b93f6099df0ba429505
h.yoshino <osushicrusher@gmail.com>
2022-01-24 00:31:53 +0900

    20220124

---
+++ 1.two-sum.ts
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=1 lang=typescript
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+function twoSum(numbers: number[], target: number): number[] {
+    const numberMap = new Map()
+    for (let i = 0; i < numbers.length; i++) {
+        const number = numbers[i]
+        const difference = target - number
+        if (numberMap.has(difference)) return [numberMap.get(difference), i]
+        numberMap.set(number, i)
+    }
+};
+// @lc code=end
+
